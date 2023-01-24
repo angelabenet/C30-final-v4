eee18fea6927205b5778547059fc6ab473f7e84b
ab <97624857+angelabenet@users.noreply.github.com>
2023-01-24 15:42:21 -0300

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -38,8 +38,6 @@ class CannonBall {
       this.trajectory.push(position);
     }
 
-    for (var i = 0; i < this.trajectory.length; i++) {
-      image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
-    }
+// add loop
   }
 }
